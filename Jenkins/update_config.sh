3ab0c80adc55be26f81ff03ecd2f14a7ba097378
elafkaihi <elmehdi.lafkaihi@centrale-casablanca.ma>
2024-11-05 12:06:14 +0100

    update config_ update

--- Jenkins/update_config.sh
+++ Jenkins/update_config.sh
@@ -15,9 +15,9 @@ efs_id=$5
 
 # Update cluster.yaml using yq
 yq e -i ".vpc.id = \"${vpc_id}\"" cluster.yaml
-yq e -i ".vpc.subnets.public.\"eu-west-3a\"[0] = \"${public_subnet_id}\"" cluster.yaml
-yq e -i ".vpc.subnets.private.\"eu-west-3a\"[0] = \"${private_subnet_1_id}\"" cluster.yaml
-yq e -i ".vpc.subnets.private.\"eu-west-3b\"[0] = \"${private_subnet_2_id}\"" cluster.yaml
+yq e -i ".vpc.subnets.public.\"eu-west-3a\".id = \"${public_subnet_id}\"" cluster.yaml
+yq e -i ".vpc.subnets.private.\"eu-west-3a\".id = \"${private_subnet_1_id}\"" cluster.yaml
+yq e -i ".vpc.subnets.private.\"eu-west-3b\".id = \"${private_subnet_2_id}\"" cluster.yaml
 
 # Update values.yaml for EFS
 yq e -i ".storage.efs.fileSystemId = \"${efs_id}\"" values.yaml
